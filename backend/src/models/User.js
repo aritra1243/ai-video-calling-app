51e162fee9599c5471cfec8c6c83d434a53760f3
aritra1243 <dhabalaritra2001@gmail.com>
2026-08-10 17:24:03 +0530

    Fix Mongoose async pre-save hook signature in User.js

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -31,11 +31,10 @@ const userSchema = new mongoose.Schema({
 });
 
 // Hash password before saving
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('passwordHash')) return next();
-  const salt = await bcrypt.genSalt(12);
+userSchema.pre('save', async function () {
+  if (!this.isModified('passwordHash')) return;
+  const salt = await bcrypt.genSalt(10);
   this.passwordHash = await bcrypt.hash(this.passwordHash, salt);
-  next();
 });
 
 // Compare password method
